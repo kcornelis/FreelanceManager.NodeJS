7a2c61923acfbae1796b9f67cb7dced08b9a829b
Kevin Cornelis <kevin.cornelis@outlook.com>
2014-10-30 18:17:35 +0100

    Redirect to login page if not authenticated

--- public/modules/account/config/authenticate.client.config.js
+++ public/modules/account/config/authenticate.client.config.js
@@ -1,31 +1,31 @@
 'use strict';
 
-angular.module('account').factory('authInterceptor', function ($rootScope, $q, $window) {
-  return {
-    request: function (config) {
-      config.headers = config.headers || {};
-      if ($window.sessionStorage.token) {
-        config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
-      }
-      return config;
-    },
-    response: function (response) {
-      if (response.status === 401) {
-        // handle the case where the user is not authenticated
-      }
-      return response || $q.when(response);
-    }
-  };
-});
-
-angular.module('account').config(function ($httpProvider) {
-  $httpProvider.interceptors.push('authInterceptor');
-});
-
-angular.module('account').run(function($rootScope, $location, $window) {
-    $rootScope.$on('$stateChangeStart', function(event, nextRoute, currentRoute) {
-        if (nextRoute.access.requiredLogin && !$window.sessionStorage.token) {
-            $location.path("/login");
-        }
-    });
+angular.module('account', ['angular-jwt'])
+.factory('authInterceptor', function ($rootScope, $q, $window) {
+	return {
+		request: function (config) {
+			config.headers = config.headers || {};
+			if ($window.sessionStorage.token) {
+				config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+			}
+			return config;
+		},
+		response: function (response) {
+			if (response.status === 401) {
+				// handle the case where the user is not authenticated
+			}
+			return response || $q.when(response);
+		}
+	};
+})
+.config(function ($httpProvider) {
+	$httpProvider.interceptors.push('authInterceptor');
+})
+.run(function($rootScope, $location, $window, jwtHelper) {
+		$rootScope.$on('$stateChangeStart', function(event, nextRoute, currentRoute) {
+				var loggedIn = $window.sessionStorage.token && !jwtHelper.isTokenExpired($window.sessionStorage.token);
+				if (nextRoute.access.requiredLogin && !loggedIn) {
+						$location.path("/login");
+				}
+		});
 });
