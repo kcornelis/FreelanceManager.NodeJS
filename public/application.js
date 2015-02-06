db6dbc8b329086f9b49453f161d9f885f2a3112f
Kevin Cornelis <kevin.cornelis@outlook.com>
2015-02-06 07:09:27 +0100

    Azure watch files for changes

--- public/application.js
+++ public/application.js
@@ -1,5 +1,3 @@
-'use strict';
-
 //Start by defining the main module and adding the module dependencies
 angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
 
