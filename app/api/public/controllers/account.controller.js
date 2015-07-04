246ef3109596c6e46608b0f8a858d146f932c39e
Kevin Cornelis <kevin.cornelis@outlook.com>
2015-07-04 13:28:58 +0200

    Account api with promises

--- app/api/public/controllers/account.controller.js
+++ app/api/public/controllers/account.controller.js
@@ -1,35 +1,35 @@
 'use strict';
 
-var mongoose = require('mongoose'),
+var mongoose = require('mongoose-q')(),
 	convert = require('../converters/account'),
 	Account = mongoose.model('Account');
 
 exports.getById = function(req, res, next) {
 
-	Account.findById(req.params.accountId)
-		.exec(function(err, account) {
-			if(err) next(err);
-			else res.send(convert(account));
-		});
+	Account.findByIdQ(req.params.accountId)
+		.then(convert.toDtoQ)
+		.then(res.send.bind(res))
+		.catch(next)
+		.done();
 };
 
 exports.getAll = function(req, res, next) {
 
-	Account.find()
-		.exec(function(err, accounts) {
-			if(err) next(err);
-			else res.send(convert(accounts));
-		});
+	Account.findQ()
+		.then(convert.toDtoQ)
+		.then(res.send.bind(res))
+		.catch(next)
+		.done();
 };
 
 exports.create = function(req, res, next) {
 
 	var account = Account.create(req.body.name, req.body.firstName, req.body.lastName, req.body.email);
-	account.save(function(err) {
-
-		if(err) next(err); 
-		else res.send(convert(account));
-	});
+	account.saveQ()
+		.then(convert.toDtoQ)
+		.then(res.send.bind(res))
+		.catch(next)
+		.done();
 };
 
 exports.update = function(req, res, next) {	
@@ -37,21 +37,19 @@ exports.update = function(req, res, next) {
 	if(req.user.id !== req.params.accountId)
 		return next();
 
-	Account.findById(req.params.accountId)
-		.exec(function(err, account) {
-
-			if(err) { next(err); }
-			else if(account) {
-
+	Account.findByIdQ(req.params.accountId)
+		.then(function(account) {
+			if(account) {
 				account.changeDetails(req.body.name, req.body.firstName, req.body.lastName, req.body.email);
-				account.save(function(err) {
-
-					if(err) next(err);
-					else res.send(convert(account));
-				});
+				return account.saveQ().then(convert.toDtoQ);
 			}
-			else next();
-		});
+		})
+		.then(function(dto) { 
+			if(dto) res.send(dto);
+			else next(); // account not found
+		})
+		.catch(next)
+		.done();
 };
 
 exports.changepassword = function(req, res, next) {
@@ -59,19 +57,17 @@ exports.changepassword = function(req, res, next) {
 	if(req.user.id !== req.params.accountId)
 		return next();
 
-	Account.findById(req.params.accountId)
-		.exec(function(err, account) {
-
-			if(err) { next(err); }
-			else if(account && account.authenticate(req.body.oldPassword)) {
-		
+	Account.findByIdQ(req.params.accountId)
+		.then(function(account) {
+			if(account && account.authenticate(req.body.oldPassword)) {
 				account.changePassword(req.body.newPassword);
-				account.save(function(err) {
-
-					if(err) next(err);
-					else res.send({ ok: true });
-				});
+				return account.saveQ().then(convert.toDtoQ);
 			}
-			else next();
-		});
+		})
+		.then(function(dto) { 
+			if(dto) res.send({ ok: true });
+			else next(); // account not found
+		})
+		.catch(next)
+		.done();
 };
